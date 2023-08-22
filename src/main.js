8bc2cfcaa69c91f8e29cf47dffab2b4c1838d073
sai <saivivek1414@gmail.com>
2023-08-22 10:38:54 +0530

    \/

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import Chats from './components/chats'
 import logo from './utils/whatsappweblogo.png'
 
 const Main=()=>{
-   
     const[name,setName]=useState()
     const[dp,setDP]=useState()
     const groupname=(data)=>{
@@ -23,8 +22,8 @@ const Main=()=>{
             <div className='chatsmain'>
                 <img style={{width:'32vw',height:'36vh',marginTop:'22vh',marginLeft:'18vw'}} src={logo} alt=''></img>
                 <h1 style={{color:'white',fontWeight:'100',textAlign:'center'}}>WhatsApp Marketplace</h1>
-                <h4 style={{color:'white',fontWeight:'100',textAlign:'center'}}>Send and receive messages without keeping your phone online.</h4>
-                <h4 style={{color:'white',fontWeight:'100',textAlign:'center'}}>Use WhatsApp on up to 4 linked devices and 1 phone at the same time.</h4>
+                <h4 style={{color:'white',fontWeight:'100',textAlign:'center'}}>An online platform within WhatsApp where users can buy, sell, and trade products and services with other users</h4>
+                <h4 style={{color:'white',fontWeight:'100',textAlign:'center'}}>Use WhatsApp marketplace on up to 4 linked devices and 1 phone at the same time.</h4>
             </div>}
         </div>
     )
